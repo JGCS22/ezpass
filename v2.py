5ca72f8607ea44bba2f2ae819abc549a4dbecf47
JGCS22 <115753611+JGCS22@users.noreply.github.com>
2024-09-14 11:44:22 -0400

    Update

--- v2.py
+++ v2.py
@@ -1,41 +1,40 @@
 from imports import *
 
-menu = ["EZPASS", "", "LOGIN", "RESET", "EXIT"]
-
-def print_menu(stdscr, selected_row):
+def print_display(stdscr, display_menu, current_selection):
     stdscr.clear()
-    for idx, row in enumerate(menu):
-        if idx == selected_row:
-            stdscr.addstr(idx, 0, row, curses.A_REVERSE)  # Highlight current row
+    for menu_item in display_menu:
+        current_index = display_menu.index(menu_item)
+        if current_index == current_selection:
+            stdscr.addstr(current_index, 0, menu_item[0], curses.A_REVERSE)
         else:
-            stdscr.addstr(idx, 0, row)
+            stdscr.addstr(current_index, 0, menu_item[0])
     stdscr.refresh()
 
+
 def main(stdscr):
-    curses.curs_set(0) #Set invis cursor for title of app
-    print_menu(stdscr, 2) #print intial menu
-    current_row = 2
+    display_menu = [["EZPASS", 0], ["", 0], ["LOGIN", 1], ["RESET", 1], ["BACK", 1]]
+    current_selection = 2
+    menu_state = 0
+
+    username_attempt = ""
+    master_password_attempt = ""
+
+    curses.curs_set(0)
+    print_display(stdscr, display_menu, current_selection)
 
     while True:
-        key_pressed = stdscr.getch()
-        
-        if key_pressed == curses.KEY_UP and current_row > 2:
-            current_row -= 1
-        elif key_pressed == curses.KEY_DOWN and current_row < len(menu) - 1:
-            current_row += 1
+        if menu_state == 0:
+            key_pressed = stdscr.getch()
+            if key_pressed == curses.KEY_UP and current_selection > 2:
+                current_selection -= 1
+            elif key_pressed == curses.KEY_DOWN and current_selection < len(display_menu) - 1:
+                current_selection += 1
+                
+            print_display(stdscr, display_menu, current_selection)
 
-        print_menu(stdscr, current_row)
-        
-        if key_pressed == curses.KEY_ENTER or key_pressed in [10, 13]:
-            if current_row == 2:
-                curses.curs_set(1)
-                stdscr.addstr(2, 0, "USERNAME: ")
-                stdscr.addstr(2, 0, "PASSWORD: ")
-                curses.echo()
-                username_attempt = stdscr.getstr(2, 10, 50)
-                master_password_attempt = stdscr.getstr(3, 10, 50)
-                curses.curs_set(0)
-                stdscr.refresh()
+            if key_pressed == curses.KEY_ENTER or key_pressed in [10, 13]:
+                if current_selection == 2:
+                    
 
 if __name__ == "__main__":
     curses.wrapper(main)
